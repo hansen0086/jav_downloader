27068824f439842f123db4a155f44f353408fcac
Hansen Liu <hansen0086@163.com>
2019-06-11 03:36:13 +1000

    Update jav_downloader.py
    added special cases , now supplies on the old version of the website as well

--- jav_downloader.py
+++ jav_downloader.py
@@ -3,9 +3,10 @@ import re
 import os
 from bs4 import BeautifulSoup
 
-key_word = "天海翼" # the searching keyword
+key_word = "深田" # the searching keyword
 resolution = "480p"# can choose 480p or 720p
 new_folder_name = key_word
+
 try:
     os.mkdir(new_folder_name)
 except:
@@ -23,9 +24,6 @@ def getHTMLText(url):
         return "Error in status"
 
 
-
-
-
 base_url = "http://avjoy.me/search/videos?search_query="+key_word
 
 #mp4_url = "http://avjoy.me:2087/video/Js29eUN1b2yt3YwGrrHxQw/1560169188/1871_480p.mp4"
@@ -40,13 +38,39 @@ container = wrapper.find('div',class_='container')
 row = container.find('div',class_='row')
 sm8= row.find('div',class_='col-md-9 col-sm-8')
 row2 = sm8.find('div',class_='row')
-
+row3 = row2
 #print(row2)
 
 video_counter =0
 
-#for each video page in this search result page
+#print all the urls
 for instance in row2.find_all('div',class_='col-sm-6 col-md-4 col-lg-4'):
+    video_counter+=1
+    href = instance.find('div',class_='well well-sm').a['href']#video name
+    videopageurl = "http://avjoy.me"+href #the url of the video page
+
+    text = getHTMLText(videopageurl)
+    soup = BeautifulSoup(text,'lxml')
+    row2 = soup.find('div',id='wrapper').find('div',class_='container').find_all('div',class_='row')[1]
+    #print(row2.find('div',class_='col-md-8').find('div').find('div',class_='video-container'))
+    try:
+        #new version has property where label="480p" and type="video/mp4"
+        mp4url= row2.find('div',class_='col-md-8').find('div').find('div',class_='video-container').find('video').find('source',label=resolution)['src']
+    except:
+        mp4url= row2.find('source')['src']
+    #print(mp4url)
+    file_name = href.split('/')[3]+".mp4"
+
+    print("got %s from %s"% (file_name,mp4url))
+
+print("%d videos in total"%(video_counter))
+
+
+video_counter =0
+#for each video page in this search result page
+for instance in row3.find_all('div',class_='col-sm-6 col-md-4 col-lg-4'):
+    #print("in for loop")
+
     video_counter+=1
     href = instance.find('div',class_='well well-sm').a['href']#video name
     videopageurl = "http://avjoy.me"+href #the url of the video page
@@ -54,30 +78,23 @@ for instance in row2.find_all('div',class_='col-sm-6 col-md-4 col-lg-4'):
     #print(videopageurl)
     text = getHTMLText(videopageurl)
     soup = BeautifulSoup(text,'lxml')
-    row2 = soup.find('div',id='wrapper').find('div',class_='container').find_all('div',class_='row')[1]
-    mp4url= row2.find('div',class_='col-md-8').find('div').find('div',class_='video-container').find('video').find('source',label=resolution)['src']
+    row3 = soup.find('div',id='wrapper').find('div',class_='container').find_all('div',class_='row')[1]
+    #print(row3.find('div',class_='col-md-8').find('div').find('div',class_='video-container'))
+    try:
+        #new version has property where label="480p" and type="video/mp4"
+        mp4url= row3.find('div',class_='col-md-8').find('div').find('div',class_='video-container').find('video').find('source',label=resolution)['src']
+    except:
+        mp4url= row3.find('source')['src']
     #print(mp4url)
     file_name = href.split('/')[3]+".mp4"
     print("Downloading:")
-    print(file_name)
+    print("%s from %s"% (file_name,mp4url))
+    
     r = requests.get(mp4url)
-
     with open (new_folder_name+"/"+file_name,'wb') as fd:
         fd.write(r.content)
 
-    #now use the mp4url to download the mp4 file
+    
 
 print("%d videos downloaded" %(video_counter))
-
-
- 
-
-
-
-
-
-
-
-
-
 
